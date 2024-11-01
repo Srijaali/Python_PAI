c8d30f5232aaa2fe1c460de509489735c14a6de6
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-11-01 17:27:17 +0500

    Create q5.py

---
+++ assignment_theory/assignment 2/q5.py
@@ -0,0 +1,7 @@
+# Create a NumPy array of 10 values and apply the square root function to each element.
+
+import numpy as np
+arr = np.arange(0,10)
+sq = np.sqrt(arr)
+print(f"Square root: {sq}")
+
